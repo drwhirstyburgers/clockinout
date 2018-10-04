9d28f1f010a54303bc8b2006c0708cbd4219f3b2
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-10-04 16:26:55 -0400

    controller functions updated

--- app/controllers/timeclock_controller.rb
+++ app/controllers/timeclock_controller.rb
@@ -4,5 +4,24 @@ class TimeclockController < ApplicationController
   end
 
   def new
+    @teacher = Teacher.new
+  end
+
+  def create
+    @teacher = Teacher.new(sign_in_params)
+
+    if @teacher.save
+      flash[:notice] = "You have clocked in/out at #{Time.now}"
+      render :index
+    else
+      flash.now[:alert] = "There was an error, please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def sign_in_params
+    params.require(:teachers).permit(:name, :time_in, :time_out)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'timeclock/index'
 
-  get 'timeclock/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'timeclock#new'
 end
