057c80c605093815037b0eaa7de261f943aad8d9
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-10-26 11:03:37 -0400

    working out how to change this

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :teachers, only: [:new, :create]
+  resources :teachers, only: [:new, :create, :show, :update]
 
   get '/teachers', to: 'teachers#index'
 
